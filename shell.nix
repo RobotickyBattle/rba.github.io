2b8f6461defbaac578042fb7fdcf7b0ca5c5533c
H00N24 <ondrej.kurak.h@gmail.com>
2022-03-01 22:07:03 +0100

    Gem update

--- shell.nix
+++ shell.nix
@@ -5,17 +5,11 @@ with import sources.nixpkgs {};
 mkShell {
   name = "rba-github-env";
   buildInputs = [
-    ruby
-    libiconv
+    jekyll
+    bundler
     glibcLocales
-    docker-compose
-    gcc
-    gnumake
   ];
   shellHook = ''
-  # set SOURCE_DATE_EPOCH so that we can use python wheels
-  export SOURCE_DATE_EPOCH=315532800
-  export LD_LIBRARY_PATH=${stdenv.lib.makeLibraryPath [stdenv.cc.cc]}
   '';
   preferLocalBuild = true;
 }
